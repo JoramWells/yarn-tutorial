60edc19a763c5674fa2d79b9a2984ac2f23e05d9
bramuel <jorammanoah1@gmail.com>
2021-03-13 01:10:12 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.resolve('./src/views'))
+
+//Body parsing
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//create express router
+app.use(router)
+
+const routePath = path.resolve('./dist')
+app.use(express.static(routePath))
+
+
+router.use((err, req, res, next) =>{
+    if(err){
+        return res.send(err.message)
+    }
+})
+
+
+app.listen(3000, ()=>{
+    console.log('App running on http://localhost:3000')
+})
